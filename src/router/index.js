1fcfb2995b5593b894682fb1e8a2c7438475a99a
HUIKIM <969791392@qq.com>
2020-09-02 17:47:09 +0800

    update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import DashboardPage from "@/components/DashboardPage.vue";
 import Goods from "@/components/Goods/GoodsPage.vue";
+import Orders from "@/components/Order/OrdersPage.vue";
 
 Vue.use(VueRouter);
 
@@ -15,6 +16,11 @@ const routes = [
         path: 'goods',
         name: 'goods',
         component: Goods
+      },
+      {
+        path: 'orders',
+        name: 'orders',
+        component: Orders
       },
     ]
   }
